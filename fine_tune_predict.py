41bd6f149266dd4b21143baceb6b753d6aca2d39
TJTP <pangtt2018@gmail.com>
2020-11-10 21:50:30 +0800

    Modified

--- fine_tune_predict.py
+++ fine_tune_predict.py
@@ -1,12 +1,15 @@
 import argparse
-from pytorch_transformers.tokenization_bert import BertTokenizer 
-import torch 
 import numpy as np 
 import os 
 import logging
 import random
 
+import torch
+from pytorch_transformers.tokenization_bert import BertTokenizer  
 from pytorch_transformers import BertConfig, BertForSequenceClassification, BertTokenizer
+from torch import dtype
+from torch.utils.data import TensorDataset, DataLoader
+from process_data import transfer_to_features
         
 def train():
     pass 
@@ -14,8 +17,32 @@ def train():
 def predict():
     pass 
 
-def load_data():
-    pass
+def load_data(args, tokenizer):
+    if not os.path.exists(args.output_feature_dir):
+        os.mkdir(args.output_feature_dir)
+    if not os.listdir(args.output_feature_dir):
+        train_features, tokenized_train_examples, test_features, tokenized_test_examples \
+                                                    = transfer_to_features(args, tokenizer)
+        print(train_features[0].label_id)
+        np.save(os.path.join(args.output_feature_dir, "cached_train.npy"), np.array(train_features), allow_pickle=True)
+        np.save(os.path.join(args.output_feature_dir, "cached_test.npy"), np.array(test_features), allow_pickle=True)
+
+        
+    else:
+        train_features = list(np.load(os.path.join(args.output_feature_dir, "cached_train.npy"), allow_pickle=True))
+        test_features = list(np.load(os.path.join(args.output_feature_dir, "cached_test.npy"), allow_pickle=True))
+        print(test_features[0].label_id)
+    
+    train_dataset = TensorDataset(torch.tensor([feature.input_ids for feature in train_features], dtype=torch.long),
+                                  torch.tensor([feature.input_masks for feature in train_features], dtype=torch.long), 
+                                  torch.tensor([feature.segment_ids for feature in train_features], dtype=torch.long),
+                                  torch.tensor([feature.label_id for feature in train_features], dtype=torch.long))
+
+    test_dataset = TensorDataset(torch.tensor([feature.input_ids for feature in test_features], dtype=torch.long),
+                                 torch.tensor([feature.input_masks for feature in test_features], dtype=torch.long), 
+                                 torch.tensor([feature.segment_ids for feature in test_features], dtype=torch.long),
+                                 torch.tensor([feature.label_id for feature in test_features], dtype=torch.long))
+    return train_dataset, test_dataset
 
 def fix_seed(args):
     random.seed(args.seed)
@@ -26,16 +53,18 @@ def main():
     parser = argparse.ArgumentParser()
 
     ##Required paras
-    parser.add_argument("--data_dir", default=None, type=str, required=True,
-                        help="The input data dir. Contain both train and test data.")
+    parser.add_argument("--raw_data_dir", default=None, type=str, required=True,
+                        help="The raw data dir. Contain both train and test data.")
     parser.add_argument("--model_dir", default=None, type=str, required=True,
                         help="The model dir")
-    parser.add_argument("--output_dir", default=None, type=str, required=True, 
-                        help="The output files dir")
-    parser.add_argument("--train_epoch_num", default=None, type=int, 
+    parser.add_argument("--output_model_dir", default=None, type=str, required=True, 
+                        help="The output models dir")
+    parser.add_argument("--train_epoch_num", default=3, type=int, 
                         help="Number of training epochs")
 
     ##Alternative paras
+    parser.add_argument("--max_seq_len", default=128, type=int,
+                        help="The maximum total input sequence length after tokenization.")
     parser.add_argument("--save_steps", default=50, type=int, 
                         help="Save checkpoint every given steps")
     parser.add_argument("--no_cuda", action="store_true", 
@@ -46,14 +75,16 @@ def main():
                         help="Train (fine-tune) the model.")
     parser.add_argument("--predict", action="store_true", 
                         help="Use the model to predict.")
-    parser.add_argument("--overwrite_output", action="store_true", 
-                        help="Overwrite the output files")                        
+    parser.add_argument("--overwrite_model", action="store_true", 
+                        help="Overwrite the output model")        
+    parser.add_argument("--output_feature_dir", default="./data/transformed/", 
+                        help="Set the output feature dir. Default is data/transformed")              
 
     args = parser.parse_args()
     
     
-    if os.path.exists(args.output_dir) and os.listdir(args.output_dir) and args.train and not args.overwrite_output:
-        raise ValueError("Outuput dir already exists, use --overwrite_output to overwrite.")
+    if os.path.exists(args.output_model_dir) and os.listdir(args.output_model_dir) and args.train and not args.overwrite_model:
+        raise ValueError("Outuput model dir already exists, use --overwrite_model to overwrite.")
 
     # Set device
     if args.no_cuda:
@@ -68,7 +99,7 @@ def main():
     config = BertConfig.from_pretrained(pretrained_model_name_or_path=args.model_dir)
     tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=args.model_dir)
     model = BertForSequenceClassification.from_pretrained(args.model_dir, from_tf=bool('.ckpt' in args.model_dir), config=config)
-
+    train_dateset, test_dataset = load_data(args, tokenizer)
 
     # Train
     if args.train:
---
+++ process_data.py
@@ -0,0 +1,134 @@
+import parser
+import numpy as np 
+import argparse
+import os 
+import logging 
+
+# 作业给的数据集中情感极性的id
+origin_label_id_dic = {"-1": "NEG", 
+                       "0":  "NEU",
+                       "1":  "POS", 
+                       "-5": "TEST"}
+
+# BERT模型的情感极性的id
+BERT_label_id_dic = {"NEG": 1, 
+                     "NEU": 2, 
+                     "POS": 0, 
+                     "TEST": -5} #由于训练集未给出极性, 而BERT模型必须需要一个极性, 
+                                 #所以在创建训练集的example时, 用 -5 当作它的极性
+class Example:
+    def __init__(self, example):
+        self.sentence_A = example[0]
+        self.sentence_B = example[1]
+        self.label_id = convert_label_id(example[2])
+class Feature:
+    def __init__(self, input_ids, input_masks, segment_ids, label_id):
+        self.input_ids = input_ids
+        self.input_masks = input_masks
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+
+def convert_label_id(label_id_str):
+    """
+    将作业的数据集的情感极性id转换为BERT模型的情感极性的id
+    """
+    return BERT_label_id_dic[origin_label_id_dic[label_id_str]]
+
+def transfer_to_examples(raw_data_dir):
+    train_examples = []
+    test_examples = []
+
+    for filename in os.listdir(raw_data_dir):
+        filepath = os.path.join(raw_data_dir, filename)
+        if "train" in filename:
+            with open(filepath, 'r', encoding="UTF-8") as f:
+                cnt = 0
+                line = f.readline().strip()
+                example = []
+                while line and line != '':
+                    cnt += 1
+                    example.append(line)
+                    if cnt % 3 == 0:
+                        example[0] = example[0].replace("$T$", example[1], 1)
+                        train_examples.append(Example(example))
+                        example = []
+                    line = f.readline().strip()  
+            f.close()
+            #for i in range(10):
+            #    print(train_examples[i].sentence_A, train_examples[i].sentence_B, 
+            #            train_examples[i].label_id)    
+        elif "test" in filename:
+            with open(filepath, 'r', encoding="utf-8") as f:
+                cnt = 0
+                line = f.readline().strip()
+                example = []
+                while line and line != '':
+                    cnt += 1
+                    example.append(line)
+                    if cnt % 2 == 0:
+                        example[0] = example[0].replace("$T$", example[1], 1)
+                        example.append("-5")
+                        test_examples.append(Example(example))
+                        example = []
+                    line = f.readline().strip()
+            f.close() 
+            #for i in range(10):
+            #    print(test_examples[i].sentence_A, test_examples[i].sentence_B, 
+            #            test_examples[i].label_id)
+        return train_examples, test_examples
+
+def transfer_to_features(args, tokenizer):
+    train_examples, test_examples = transfer_to_examples(args.raw_data_dir)
+    train_features = []
+    test_features = []
+    tokenized_train_examples = []
+    tokenized_test_examples = []
+
+    for example in train_examples:
+        input_ids, input_masks, segment_ids, tokens_whole = tokenize(example, tokenizer, args.max_seq_len)
+        train_features.append(Feature(input_ids, input_masks, segment_ids, example.label_id))
+        tokenized_train_examples.append(" ".join([str(token) for token in tokens_whole]))
+    
+    for example in test_examples:
+        input_ids, input_masks, segment_ids, tokens_whole = tokenize(example, tokenizer, args.max_seq_len)
+        test_features.append(Feature(input_ids, input_masks, segment_ids, example.label_id))
+        tokenized_test_examples.append(" ".join([str(token) for token in tokens_whole]))
+    
+    return train_features, tokenized_train_examples, test_features, tokenized_test_examples
+
+def tokenize(example, tokenizer, max_seq_len):
+    tokens_A = tokenizer.tokenize(example.sentence_A)
+    tokens_B = tokenizer.tokenize(example.sentence_B) 
+    
+    # 第一个句子 (不含开头指示符) 的token以及对应的segment_id
+    tokens_whole = tokens_A + ['[SEP]']
+    segment_ids = [0] * len(tokens_whole) 
+
+    #第二个句子的token以及对应的segment_id
+    tokens_whole += tokens_B + ['[SEP]']
+    segment_ids += [1] * (len(tokens_B) + 1)
+
+    #在开头加上[CLS]
+    tokens_whole = ['[CLS]'] + tokens_whole
+    segment_ids = [1] + segment_ids
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens_whole)
+
+    input_masks = [1] * len(input_ids)
+    padding_len = max_seq_len - len(input_ids)
+    # 假定不会超过最长串
+    assert padding_len > 0
+    padding = [0] * padding_len
+    # 在剩余的位置补上 0 (mask 和 id 都是)
+    input_ids = padding + input_ids
+    input_masks = padding + input_masks
+    segment_ids = padding + segment_ids
+
+    assert len(input_ids) == max_seq_len
+    assert len(input_masks) == max_seq_len
+    assert len(segment_ids) == max_seq_len
+
+    return input_ids, input_masks, segment_ids, tokens_whole
+
+if __name__ == "__main__":
+    transfer_to_examples("./data/raw/")
