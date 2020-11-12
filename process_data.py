4b11a271d6f9ca5427340518a2484a9d385f0664
TJTP <pangtt2018@gmail.com>
2020-11-12 12:13:22 +0800

    Simplize structure

--- process_data.py
+++ process_data.py
@@ -10,12 +10,6 @@ BERT_label_id_dic = {"NEG": 1,
                      "NEU": 2, 
                      "POS": 0,} 
 
-class Example:
-    def __init__(self, example):
-        self.sentence_A = example[0]
-        self.sentence_B = example[1]
-        self.label_id = convert_label_id(example[2])
-
 class Feature:
     def __init__(self, input_ids, input_masks, segment_ids, label_id):
         self.input_ids = input_ids
@@ -37,76 +31,22 @@ def convert_label_id_back(bert_results):
     bert_results[bert_results == 1] = -1
     bert_results[bert_results == 0] = 1
     bert_results[bert_results == 2] = 0
+
     return bert_results
 
-def transfer_to_examples(raw_data_dir):
-    train_examples, test_examples = [], []
+def print_features(train_features, tokenized_train_examples, test_features, tokenized_test_examples, n):
+    for i in range(n):
+        print("*** Example-%d ***"%(i))
+        print("tokens: %s" % (tokenized_train_examples[i]))
+        print("input_ids: %s" % (train_features[i].input_ids))
+        print("input_masks: %s" % (train_features[i].input_masks))
+        print("segment_ids: %s" % (train_features[i].segment_ids))
+        print("label_id = %d"% (train_features[i].label_id))
 
-    #print(os.listdir(raw_data_dir))
-    for filename in os.listdir(raw_data_dir):
-        filepath = os.path.join(raw_data_dir, filename)
-        if "train" in filename:
-            with open(filepath, 'r', encoding="UTF-8") as f:
-                cnt = 0
-                line = f.readline().strip()
-                example = []
-                while line and line != '':
-                    cnt += 1
-                    example.append(line)
-                    if cnt % 3 == 0:
-                        example[0] = example[0].replace("$T$", example[1], 1)
-                        train_examples.append(Example(example))
-                        example = []
-                    line = f.readline().strip()  
-            f.close()
-            #for i in range(10):
-            #    print(train_examples[i].sentence_A, train_examples[i].sentence_B, 
-            #            train_examples[i].label_id)    
-        elif "test" in filename:
-            with open(filepath, 'r', encoding="utf-8") as f:
-                cnt = 0
-                line = f.readline().strip()
-                example = []
-                while line and line != '':
-                    cnt += 1
-                    example.append(line)
-                    if cnt % 2 == 0:
-                        example[0] = example[0].replace("$T$", example[1], 1)
-                        # 由于BERT模型的输入必须要有一个label_id, 所以把所有
-                        # 测试集中样本的label_id都设成 "0" (neual), 在使用中
-                        # 不会用到
-                        example.append("0") 
-                        test_examples.append(Example(example))
-                        example = []
-                    line = f.readline().strip()
-            f.close() 
-            #for i in range(10):
-            #    print(test_examples[i].sentence_A, test_examples[i].sentence_B, 
-            #            test_examples[i].label_id)
-
-    return train_examples, test_examples
-
-def transfer_to_features(args, tokenizer):
-    train_examples, test_examples = transfer_to_examples(args.data_dir)
-    train_features, test_features = [], []
-    tokenized_train_examples, tokenized_test_examples = [], []
-
-    for example in train_examples:
-        input_ids, input_masks, segment_ids, tokens_whole = tokenize(example, tokenizer, args.max_seq_len)
-        train_features.append(Feature(input_ids, input_masks, segment_ids, example.label_id))
-        tokenized_train_examples.append(" ".join([str(token) for token in tokens_whole]))
-    
-    #print(train_features[0].label_id)
-    for example in test_examples:
-        input_ids, input_masks, segment_ids, tokens_whole = tokenize(example, tokenizer, args.max_seq_len)
-        test_features.append(Feature(input_ids, input_masks, segment_ids, example.label_id))
-        tokenized_test_examples.append(" ".join([str(token) for token in tokens_whole]))
-    
-    return train_features, tokenized_train_examples, test_features, tokenized_test_examples
 
 def tokenize(example, tokenizer, max_seq_len):
-    tokens_A = tokenizer.tokenize(example.sentence_A)
-    tokens_B = tokenizer.tokenize(example.sentence_B) 
+    tokens_A = tokenizer.tokenize(example[0])
+    tokens_B = tokenizer.tokenize(example[1]) 
     
     # 第一个句子 (不含开头指示符) 的token以及对应的segment_id
     tokens_whole = tokens_A + ['[SEP]']
@@ -138,6 +78,62 @@ def tokenize(example, tokenizer, max_seq_len):
     assert len(segment_ids) == max_seq_len
 
     return input_ids, input_masks, segment_ids, tokens_whole
+
+def transfer_to_features(args, tokenizer):
+    train_features, test_features = [], []
+    tokenized_train_examples, tokenized_test_examples = [], []
+
+    raw_data_dir = args.data_dir
+    for filename in os.listdir(raw_data_dir):
+        filepath = os.path.join(raw_data_dir, filename)
+        if "train" in filename:
+            with open(filepath, 'r', encoding="UTF-8") as f:
+                cnt = 0
+                line = f.readline().strip()
+                example = []
+                while line and line != '':
+                    cnt += 1
+                    example.append(line)
+                    if cnt % 3 == 0:
+                        example[0] = example[0].replace("$T$", example[1], 1)
+
+                        input_ids, input_masks, segment_ids, tokens_whole \
+                            = tokenize(example, tokenizer, args.max_seq_len)
+                        train_features.append(Feature(input_ids, input_masks, segment_ids, convert_label_id(example[2])))
+                        tokenized_train_examples.append(" ".join([str(token) for token in tokens_whole]))
+
+                        example = []
+                    line = f.readline().strip()  
+            f.close()
+            #for i in range(10):
+            #    print(train_examples[i].sentence_A, train_examples[i].sentence_B, 
+            #            train_examples[i].label_id)    
+        elif "test" in filename:
+            with open(filepath, 'r', encoding="utf-8") as f:
+                cnt = 0
+                line = f.readline().strip()
+                example = []
+                while line and line != '':
+                    cnt += 1
+                    example.append(line)
+                    if cnt % 2 == 0:
+                        example[0] = example[0].replace("$T$", example[1], 1)             
+                        example.append("0") # 由于BERT模型的输入必须要有一个label_id, 所以把所有
+                                            # 测试集中样本的label_id都设成 "0" (neual), 在使用中
+                                            # 不会用到
+                        input_ids, input_masks, segment_ids, tokens_whole \
+                            = tokenize(example, tokenizer, args.max_seq_len)
+                        test_features.append(Feature(input_ids, input_masks, segment_ids, convert_label_id(example[2])))
+                        tokenized_test_examples.append(" ".join([str(token) for token in tokens_whole]))
+
+                        example = []
+                    line = f.readline().strip()
+            f.close() 
+            #for i in range(10):
+            #    print(test_examples[i].sentence_A, test_examples[i].sentence_B, 
+            #            test_examples[i].label_id)
+
+    return train_features, tokenized_train_examples, test_features, tokenized_test_examples
 
 if __name__ == "__main__":
     #transfer_to_examples("./data/raw/")
