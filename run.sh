98ade81f007d6ade0bbc3256e1673b89810914e6
TJTP <pangtt2018@gmail.com>
2020-11-29 11:44:45 +0800

    Modified

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 python fine_tune_predict.py \
 --data_dir ./data/dataset/ \
 --model_dir ./data/models/restaurants_10mio_ep3/ \
---train --max_steps 300 --train_batch_size 32 \
+--train --train_epoch_num 6 --train_batch_size 32 \
 --warmup_steps 50 --predict --predict_batch_size 32 \
 --overwrite_model
